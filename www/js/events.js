4fd2d73a8ef98950d56f4d335a49a9687e166909
Steve Fulton <sjf1008@sru.edu>
2016-07-19 13:37:56 -0400

    Added network checks and callbacks for network connectivity.

--- www/js/events.js
+++ www/js/events.js
@@ -3,6 +3,17 @@ var coords;
 var USER_HASH_KEY = "user_hash";
 
 
+function isConnected() {
+	var result = false;
+	
+	if (navigator.connection.type != Connection.NONE) {
+		result = true;
+	}
+	
+	return result;
+}
+
+
 function generateUserHash() {
 	
 	var userHash;
@@ -60,58 +71,66 @@ function initializeFCM() {
 
 
 function requestLocation() {
-    var onSuccess = function(position) {
-        coords = position.coords;
-        console.log("got coords="+coords);
-		console.log("Latitude: " + coords.latitude);
-		console.log("Longitude: " + coords.longitude);
-		console.log("Altitude: " + coords.altitude);
-		console.log("Accuracy: " + coords.accuracy);
-		console.log("Heading: " + coords.heading);
-		console.log("Speed: " + coords.speed);
-		console.log("Timestamp: " + position.timestamp);
-    };
-	var onError = function (error) {
-		console.log("error code: " + error.code);
-		console.log("error message: " + error.message);
+	if (isConnected()) {
+		var onSuccess = function(position) {
+			coords = position.coords;
+			console.log("got coords="+coords);
+			console.log("Latitude: " + coords.latitude);
+			console.log("Longitude: " + coords.longitude);
+			console.log("Altitude: " + coords.altitude);
+			console.log("Accuracy: " + coords.accuracy);
+			console.log("Heading: " + coords.heading);
+			console.log("Speed: " + coords.speed);
+			console.log("Timestamp: " + position.timestamp);
+		};
+		var onError = function (error) {
+			console.log("error code: " + error.code);
+			console.log("error message: " + error.message);
+		}
+		navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 5000, enableHighAccuracy: true });
+	} else {
+		alert("No network connection.\n\nRequesting location requires connection to a network.");
 	}
-
-    navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 10000, enableHighAccuracy: true });
 }
 
 
 function onClickSubmit() {
-    // get input values
-    var latitude = (coords != null) ? coords.latitude : 0;
-    var longitude = (coords != null) ? coords.longitude : 0;
-    var smell_value = $("#slider_smell_value")[0].value;
-    var smell_description = $("#textfield_smell_description")[0].value;
-    var feelings_symptoms = $("#textfield_feelings_symptoms")[0].value;
-    // userHash
-	var userHash = generateUserHash();
+	if (isConnected()) {
+		// get input values
+		var latitude = (coords != null) ? coords.latitude : 0;
+		var longitude = (coords != null) ? coords.longitude : 0;
+		var smell_value = $("#slider_smell_value")[0].value;
+		var smell_description = $("#textfield_smell_description")[0].value;
+		var feelings_symptoms = $("#textfield_feelings_symptoms")[0].value;
+		// userHash
+		var userHash = generateUserHash();
 
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "http://localhost/api/v1/smell_reports",
-        data: {
-            "user_hash" : userHash,
-            "latitude" : latitude,
-            "longitude" : longitude,
-            "smell_value" : smell_value,
-            "smell_description" : smell_description,
-            "feelings_symptoms" : feelings_symptoms,
-        },
-        xhrFields: { withCredentials: false },
-        success: function(data) {
-            console.log("success!");
-            console.log(data);
-            $.mobile.changePage($("#map"), {changeHash:false, transition: "none"});
-        },
-        error: function(msg) {
-            alert(msg.message);
-        }
-    });
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "http://localhost/api/v1/smell_reports",
+			data: {
+				"user_hash" : userHash,
+				"latitude" : latitude,
+				"longitude" : longitude,
+				"smell_value" : smell_value,
+				"smell_description" : smell_description,
+				"feelings_symptoms" : feelings_symptoms,
+			},
+			xhrFields: { withCredentials: false },
+			success: function(data) {
+				console.log("success!");
+				console.log(data);
+				$.mobile.changePage($("#map"), {changeHash:false, transition: "none"});
+			},
+			error: function(msg) {
+				alert(msg.message);
+			}
+		});
+	} else {
+		alert("No network connection.\n\nSending smell reports requires connection to a network.");
+	}
+    
 }
 
 
--- www/js/init.js
+++ www/js/init.js
@@ -7,22 +7,22 @@ var isDeviceReady = false;
         this.bindEvents();
     },
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         console.log('Received Device Ready Event');
 		navigator.splashscreen.hide();
 		deviceready = true;
 		initializeFCM();
 		requestLocation();
+		var count = 0;
+		document.addEventListener('online', function() {
+			// why is this event firing twice?
+			console.log("in here");
+			requestLocation();
+		}, false);
     }
 };
 
