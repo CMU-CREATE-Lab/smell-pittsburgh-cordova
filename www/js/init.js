98301c6ea8b193db7041286456a01f59fbe92ebc
Steve Fulton <sjf1008@sru.edu>
2016-07-21 10:50:05 -0400

    network and location checks

--- www/js/init.js
+++ www/js/init.js
@@ -11,16 +11,27 @@ var isDeviceReady = false;
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("deviceready", this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        console.log('Received Device Ready Event');
-		requestLocation();
+        console.log("Received Device Ready Event");
 		isDeviceReady = true;
+		showSpinner();
+		document.addEventListener("resume", onResume, false);
+		document.addEventListener("pause", onPause, false);
+		requestLocation();
+		
+		function onResume() {
+			requestLocation();
+		}
+		
+		function onPause() {
+			$('#submitReport').attr('disabled', 'true');
+		}
     }
 };
 
