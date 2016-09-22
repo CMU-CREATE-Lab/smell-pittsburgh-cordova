3cb5175e4f5a9875b349ec8912ac30f863ca6c77
mtasota <miketasota@gmail.com>
2016-09-22 12:08:56 -0400

    version change (1.4)

--- merges/browser/js/constants.js
+++ merges/browser/js/constants.js
@@ -3,7 +3,7 @@ var Constants = {
   LOCATION_SERVICES: false,
   PLATFORM_CALLBACK_ONREADY: false,
   SUBMIT_TO_ACHD: true,
-  APP_VERSION: "1.3",
+  APP_VERSION: "1.4",
 
   // Page Ids
   STARTUP_PAGE: "startup",
